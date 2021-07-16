91832558a464b26a7f5b156727926153c1041971
HANDIKA CATUR MAHARDIKA <192410101066@mail.unej.ac.id>
2021-07-16 12:18:05 +0700

    change created status code

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -55,7 +55,7 @@ exports.getProject = asyncHandler( async (req, res, next) => {
 
     const project = await Project.create(req.body);
 
-    res.status(200).json({
+    res.status(201).json({
         success: true,
         data: project
     });
